a8de0d80ce4c912a032e2fa41a3423cf3437a119
Derek Leggo <dleggo@protonmail.com>
2025-02-02 21:01:16 -0700

    feat: Add help text

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,29 @@
 mod gen_pkg;
 mod pkg;
 
-fn main() {}
+fn main() {
+    for arg in std::env::args() {
+        match arg.as_str() {
+            "--help" | "-h" => {
+                println!(
+                    "Usage: fpkg command [additional arguments]
+
+Fpkg, package management, done right.
+
+Commands:
+    install/add    Installs packages
+    uninstall/rm   Uninstalls packages
+    run            Runs a program
+    gen-pkg        Generates a package from a directory
+    build-env      Build or refreshes a packages environment"
+                );
+                return;
+            }
+            "--version" | "-v" => {
+                println!("{}", env!("CARGO_PKG_VERSION"));
+                return;
+            }
+            _ => {} // It will just be handeled as a positional argument
+        }
+    }
+}
