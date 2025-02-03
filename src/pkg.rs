9831b1bc1ab18e9e5fe1242cd96377ca06b9782d
Derek Leggo <dleggo@protonmail.com>
2025-02-02 21:03:03 -0700

    cliff-ignore: Begin working on verifying pkg configs

--- src/pkg.rs
+++ src/pkg.rs
@@ -1,10 +1,15 @@
-use kdl::{KdlDocument, KdlError, KdlValue};
+use kdl::{KdlDocument, KdlError};
 use std::path::Path;
 
 pub fn verify_pkg_config(file: String) -> Result<(), String> {
     let doc: Result<KdlDocument, KdlError> = file.parse();
     if let Err(e) = doc {
         return Err(e.to_string());
+    }
+    let doc = doc.unwrap();
+    let name = doc.get("name");
+    if let None = name {
+        return Err(String::from("Name not found in package spec"));
     }
     Ok(())
 }
