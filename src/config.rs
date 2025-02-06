59dac4347b91ec01c4a659ce1c0800008efe76fe
Derek Leggo <dleggo@protonmail.com>
2025-02-05 17:46:28 -0700

    feat: Added config option support

---
+++ src/config.rs
@@ -0,0 +1,16 @@
+use std::path::Path;
+
+use crate::CONFIG_LOCATION;
+
+pub fn get_config_option(name: &String) -> Option<String> {
+    let cfg_loc: String = CONFIG_LOCATION.to_string();
+    let path = Path::new(&cfg_loc).join(name);
+    if !path.is_file() {
+        return None;
+    }
+    if let Ok(out) = std::fs::read_to_string(path) {
+        Some(out)
+    } else {
+        None
+    }
+}
