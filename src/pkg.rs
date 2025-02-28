6760565b4bd05e588c96312746e6ab8e6b39f451
Derek Leggo <dleggo@protonmail.com>
2025-02-28 13:17:38 -0700

    feat: Allow support for multiple version formats (`X.X.X`, `X.X`, `X`)

--- src/pkg.rs
+++ src/pkg.rs
@@ -1,6 +1,7 @@
 use anyhow::{anyhow, bail, Context, Result};
 use kdl::{KdlDocument, KdlError, KdlIdentifier};
 use std::{
+    cmp::Ordering,
     fmt::{self, Display},
     io::BufRead,
     path::Path,
@@ -47,6 +48,104 @@ impl PartialEq for PackageConfig {
             && self.version == other.version
             && self.depends == other.depends
     }
+}
+
+#[derive(PartialEq, Debug, Clone, Eq)]
+pub struct Version {
+    n1: u32,
+    n2: u32,
+    n3: u32,
+}
+
+impl Version {
+    pub fn new(n1: u32, n2: u32, n3: u32) -> Self {
+        Version { n1, n2, n3 }
+    }
+    pub fn from_str(s: &str) -> Result<Self> {
+        let mut ret = Version::new(0, 0, 0);
+        let dots = s.chars().filter(|c| *c == '.').count();
+        if dots > 2 {
+            bail!("To many numbers in version {}!", s);
+        }
+        let str_split = s.split(".").into_iter().collect::<Vec<&str>>();
+        if str_split.len() == 3 {
+            ret.n1 = index_or_err_str(&str_split, 0)?.parse()?;
+            ret.n2 = index_or_err_str(&str_split, 1)?.parse()?;
+            ret.n3 = index_or_err_str(&str_split, 2)?.parse()?;
+        } else if str_split.len() == 2 {
+            ret.n2 = index_or_err_str(&str_split, 0)?.parse()?;
+            ret.n3 = index_or_err_str(&str_split, 1)?.parse()?;
+        } else if str_split.len() == 1 {
+            ret.n3 = index_or_err_str(&str_split, 0)?.parse()?;
+        } else {
+            bail!("Not enough componenents in string {s}");
+        }
+        Ok(ret)
+    }
+
+    pub fn bump(&self) -> Self {
+        Version::new(self.n1, self.n2, self.n3 + 1)
+    }
+
+    pub fn zero() -> Self {
+        Version::new(0, 0, 0)
+    }
+}
+
+impl Ord for Version {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        if self.n1 > other.n1 {
+            Ordering::Greater
+        } else if self.n1 < other.n1 {
+            Ordering::Less
+        } else if self.n2 > other.n2 {
+            Ordering::Greater
+        } else if self.n2 < other.n2 {
+            Ordering::Less
+        } else if self.n3 > other.n3 {
+            Ordering::Greater
+        } else if self.n3 < other.n3 {
+            Ordering::Less
+        } else {
+            Ordering::Equal
+        }
+    }
+}
+
+impl PartialOrd for Version {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(if self.n1 > other.n1 {
+            Ordering::Greater
+        } else if self.n1 < other.n1 {
+            Ordering::Less
+        } else if self.n2 > other.n2 {
+            Ordering::Greater
+        } else if self.n2 < other.n2 {
+            Ordering::Less
+        } else if self.n3 > other.n3 {
+            Ordering::Greater
+        } else if self.n3 < other.n3 {
+            Ordering::Less
+        } else {
+            Ordering::Equal
+        })
+    }
+}
+
+impl Display for Version {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}.{}.{}", self.n1, self.n2, self.n3)
+    }
+}
+
+//impl pubgrub::version::Version for Version {}
+
+pub fn index_or_err_str(s: &Vec<&str>, i: u32) -> Result<String> {
+    Ok((s
+        .to_owned()
+        .get(i as usize)
+        .ok_or(anyhow!("Failed to get index {i}, from string {:#?}", s))?)
+    .to_string())
 }
 
 /// Reads a kdl value from a kdl document, bailing if it is not a string or doesn't exist.
@@ -155,7 +254,7 @@ pub fn string_to_package(s: &str) -> Result<Package> {
         .split("-")
         .last()
         .ok_or(anyhow!("Failed to parse version from string {}", s))?;
-    pubgrub::version::SemanticVersion::from_str(version)
+    Version::from_str(version)
         .context(anyhow!("Failed to parse version string {}", version))?;
     let name: Vec<String> = s.split("-").map(|x| x.to_string() + "-").collect(); // Collect each segment, adding a '-' after each
     let name = &name[..name.len() - 1].concat(); // Concat each segment
@@ -240,5 +339,31 @@ depends python version="^8.9.112"
                 version: "0.4.3".to_string()
             }
         );
+    }
+
+    #[test]
+    fn test_version_from_str() {
+        assert_eq!(Version::from_str("0.0.0").unwrap(), Version::new(0, 0, 0));
+        assert_eq!(Version::from_str("1.2.3").unwrap(), Version::new(1, 2, 3));
+        assert_eq!(
+            Version::from_str("300.22.11").unwrap(),
+            Version::new(300, 22, 11)
+        );
+        assert_eq!(
+            Version::from_str("12.11").unwrap(),
+            Version::new(0, 12, 11)
+        );
+        assert_eq!(Version::from_str("531").unwrap(), Version::new(0, 0, 531));
+        assert_eq!(Version::from_str("13.1").unwrap(), Version::new(0, 13, 1));
+    }
+
+    #[test]
+    fn test_version_cmp() {
+        assert!(Version::new(0, 1, 2) > Version::new(0, 1, 1));
+        assert!(Version::new(6, 5, 4) > Version::new(0, 22, 500));
+        assert!(Version::new(98, 54, 97) == Version::new(98, 54, 97));
+        assert!(Version::new(98, 54, 97) >= Version::new(98, 54, 97));
+        assert!(Version::new(0, 0, 2) < Version::new(0, 0, 3));
+        assert!(!(Version::new(0, 0, 2) < Version::new(0, 0, 2)));
     }
 }
