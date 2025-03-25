55504bd4e5d54722151ad7270d094aea2bce0828
Derek Leggo <dleggo@protonmail.com>
2025-03-25 14:49:09 -0600

    feat: Add login.defs file to base

--- src/base.rs
+++ src/base.rs
@@ -9,6 +9,20 @@ fn mkdir_p(d: &Path) -> Result<()> {
 }
 
 fn rebuild_base_(dpt: &DptFile, base_dir: &Path) -> Result<()> {
+    build_directory_structure(&base_dir)?;
+
+    let passwd = build_passwd(&dpt);
+    std::fs::write(base_dir.join("etc/passwd"), passwd)?;
+
+    let group = build_group(&dpt);
+    std::fs::write(base_dir.join("etc/group"), group)?;
+
+    let login_dot_defs = build_login_dot_defs();
+    std::fs::write(base_dir.join("etc/login.defs"), login_dot_defs)?;
+    Ok(())
+}
+
+fn build_directory_structure(base_dir: &Path) -> Result<()> {
     mkdir_p(&base_dir)?;
     mkdir_p(&base_dir.join("usr/bin"))?;
     mkdir_p(&base_dir.join("usr/lib"))?;
@@ -19,7 +33,10 @@ fn rebuild_base_(dpt: &DptFile, base_dir: &Path) -> Result<()> {
     symlink("usr/bin", &base_dir.join("sbin"))?;
     symlink("bin", &base_dir.join("usr/sbin"))?;
     symlink("lib", &base_dir.join("usr/lib64"))?;
+    Ok(())
+}
 
+fn build_passwd(dpt: &DptFile) -> String {
     let mut passwd = String::new();
     for user in dpt.users.iter() {
         passwd.push_str(&format!(
@@ -32,8 +49,10 @@ fn rebuild_base_(dpt: &DptFile, base_dir: &Path) -> Result<()> {
             user.shell
         ));
     }
-    std::fs::write(base_dir.join("etc/passwd"), passwd)?;
+    passwd
+}
 
+fn build_group(dpt: &DptFile) -> String {
     let mut group = String::new();
     for g in dpt.groups.iter() {
         let empty_string = String::new();
@@ -47,9 +66,69 @@ fn rebuild_base_(dpt: &DptFile, base_dir: &Path) -> Result<()> {
         }
         group.push_str(&format!("{}:*:{}:{}", g.groupname, g.gid, members_str));
     }
+    group
+}
 
-    std::fs::write(base_dir.join("etc/group"), group)?;
-    Ok(())
+fn build_login_dot_defs() -> String {
+    r#"
+FAIL_DELAY            3
+FAILLOG_ENAB          yes
+LOG_UNKFAIL_ENAB      no
+LOG_OK_LOGINS         no
+LASTLOG_ENAB          yes
+MAIL_CHECK_ENAB       yes
+OBSCURE_CHECKS_ENAB   yes
+PORTTIME_CHECKS_ENAB  yes
+QUOTAS_ENAB           yes
+SYSLOG_SU_ENAB        yes
+SYSLOG_SG_ENAB        yes
+CONSOLE               /etc/securetty
+MOTD_FILE             /etc/motd
+FTMP_FILE             /var/log/btmp
+NOLOGINS_FILE         /etc/nologin
+SU_NAME               su
+MAIL_DIR              /var/mail
+HUSHLOGIN_FILE        .hushlogin
+ENV_HZ                HZ=100
+ENV_SUPATH            PATH=/usr/sbin:/usr/bin
+ENV_PATH              PATH=/usr/bin
+TTYGROUP              tty
+TTYPERM               0600
+ERASECHAR             0177
+KILLCHAR              025
+UMASK                 022
+PASS_MAX_DAYS         99999
+PASS_MIN_DAYS         0
+PASS_MIN_LEN          5
+PASS_WARN_AGE         7
+SU_WHEEL_ONLY         no
+UID_MIN               1000
+UID_MAX               60000
+SYS_UID_MIN           101
+SYS_UID_MAX           999
+SUB_UID_MIN           100000
+SUB_UID_MAX           600100000
+SUB_UID_COUNT         65536
+GID_MIN               1000
+GID_MAX               60000
+SYS_GID_MIN           101
+SYS_GID_MAX           999
+SUB_GID_MIN           100000
+SUB_GID_MAX           600100000
+SUB_GID_COUNT         65536
+LOGIN_RETRIES         5
+LOGIN_TIMEOUT         60
+PASS_CHANGE_TRIES     5
+PASS_ALWAYS_WARN      yes
+CHFN_AUTH             yes
+CHFN_RESTRICT         rwh
+ENCRYPT_METHOD        YESCRYPT
+DEFAULT_HOME          yes
+NONEXISTENT           /nonexistent
+ENVIRON_FILE          /etc/environment
+USERGROUPS_ENAB       yes
+PREVENT_NO_AUTH       superuser"#
+        .to_string()
 }
 
 pub fn rebuild_base(dpt: &DptFile) -> Result<()> {
